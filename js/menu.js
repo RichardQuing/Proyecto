e3aacc8bec7e5f4108cd7ffd486d5c53b9b07973
RichardQuing <richardquinaucho65@gmail.com>
2024-01-21 18:49:04 -0500

    Correccion

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+/* const openModal =document.querySelector('.botonAbrir');
+const modal=document.querySelector('.modal')
+openModal.addEventListener('click',()=>{
+    modal.classList.add('.modal--show');
+
+})
+
+
+ */
