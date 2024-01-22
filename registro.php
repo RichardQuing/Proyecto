c31cd42ab5fcce3ae9a9011f0e3e1fb38425d645
RichardQuing <richardquinaucho65@gmail.com>
2024-01-21 19:32:22 -0500

    registro corec

--- registro.php
+++ registro.php
@@ -1,7 +1,7 @@
 <?php
 include 'conexion.php';
-$nombre = $_POST['Nombre'];
-$apellido = $_POST['Apellido'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
 $email = $_POST['email'];
 $fecha_reserva = $_POST['fecha'];
 $numero_personas = $_POST['personas'];
@@ -10,8 +10,8 @@ $consulta="INSERT INTO usuario (nombre, apellido, email, fecha_reserva, numero_p
 VALUES ('$nombre', '$apellido ', '$email', '$fecha_reserva', '$numero_personas')";
 $resultado=mysqli_query($conexion, $consulta);
 if($resultado){
-    echo "REGISTRO EXITOSO"
+    echo "REGISTRO EXITOSO";
 }else{
-    echo "No se pudo registrar"
+    echo "No se pudo registrar";
 }
 ?>
